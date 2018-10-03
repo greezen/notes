7f456303d55441f6996d75878ea706190ddb10a9
greezen <852820276@qq.com>
2018-10-03 15:14:31 +0800

    Add files via upload

--- go/calc/calc.go
+++ go/calc/calc.go
@@ -1,1 +1,17 @@
+package calc
 
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("this is calc init")
+}
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Minus(a, b int) int {
+	return a - b
+}
